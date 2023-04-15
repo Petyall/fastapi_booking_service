18d8640ee74b9295fb117d00313f1d33cb550382
petyal <alekolen35@gmail.com>
2023-04-15 11:56:26 +0300

    DB connected

---
+++ app/database.py
@@ -0,0 +1,13 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from app.config import settings
+
+
+# Создание движка для работы
+engine = create_async_engine(settings.DATABASE_URL)
+# Создание сессии работы с БД (работает по типу транзакции, т.е. если началось
+# какое-то действие, то оно должно завершиться. если этого не произошло, то происходит откат)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+# Класс для наследоавния моделей и создания миграций
+class Base(DeclarativeBase):
+    pass
